118ef9408c69026423fabc7648a7fd9a8820454a
71summernight <71summernight@naver.com>
2022-02-03 02:50:45 +0900

    vue-instagram

---
+++ src/components/Container.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <Post  v-for="(a,i) in postdata" :key="i" :postdata="postdata[i]" />
+</div>    
+</template>
+
+
+<script>
+import Post from './Post.vue'
+export default {
+  components:{
+      Post
+  },
+  props:{
+      postdata:Array
+  }
+}
+</script>
